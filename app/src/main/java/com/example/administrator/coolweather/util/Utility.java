0e0be8d79b825b1a0e72d728301d4cf9741f3208
dbstr <dbstr@sina.com>
2019-02-24 17:28:48 +0800

    完完成天气界面功能

--- app/src/main/java/com/example/administrator/coolweather/util/Utility.java
+++ app/src/main/java/com/example/administrator/coolweather/util/Utility.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 import com.example.administrator.coolweather.db.City;
 import com.example.administrator.coolweather.db.County;
 import com.example.administrator.coolweather.db.Province;
+import com.example.administrator.coolweather.gson.Weather;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -83,8 +84,7 @@ public class Utility {
 
     /**
      * 将返回的JSON数据解析成Weather实体类
-     *//*
-
+     */
     public static Weather handleWeatherResponse(String response) {
         try {
             JSONObject jsonObject = new JSONObject(response);
@@ -95,6 +95,6 @@ public class Utility {
             e.printStackTrace();
         }
         return null;
-    }*/
+    }
 
 }
